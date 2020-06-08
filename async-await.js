3044d20182808a1670c090528dab4873657ff537
GummiCode <gummicodeblog@gmail.com>
2020-06-08 23:02:21 +0100

    Finished up to section 4. Finished async/await.

--- async-await.js
+++ async-await.js
@@ -1,45 +1,47 @@
 //////////////////////////////////////////////////////////////////////
 // Async/Await
 
-
 // Async/Await is another way of handling promises.
 // They use promises written in the usual way. Here are some Promises. We'll set up Async/Await to handle them further down.
 
-const getIDs = new Promise ((resolve, reject) => {
+const getIDs = new Promise((resolve, reject) => {
   setTimeout(() => {
-      const outcome = Math.floor(Math.random() *10);
-      console.log(`<Recipe value: ${outcome}>`);
-      outcome >2 ? 
-          resolve([1, 2, 3, 4, 'Benji'])
-      : 
-          reject(["Error"]);
+    const outcome = Math.floor(Math.random() * 10);
+    console.log(`<Recipe value: ${outcome}>`);
+    outcome > 2 ? resolve([1, 2, 3, 4, "Benji"]) : reject(["Error"]);
   }, 1500);
 });
 
-const getRecipe = recipeID => {
+const getRecipe = (recipeID) => {
   return new Promise((resolve, reject) => {
-      setTimeout(ID => {
-         const recipe = {
-             title: "Swamp Peef with Cheese",
-             publisher: "Tseng"
-         };
-         resolve({
-             text: `${ID}: ${recipe.title}`,
-             publisher: recipe.publisher
-         });
-     }, 1500, recipeID);
- });
-};
-
-
-const getPublisher = publisher =>  {
-  return new Promise((resolve, reject) => {
-      setTimeout(pub => {
-          resolve(`Published by ${pub}`);
-      }, 1500, publisher)
+    setTimeout(
+      (ID) => {
+        const recipe = {
+          title: "Swamp Peef with Cheese",
+          publisher: "Tseng",
+        };
+        resolve({
+          text: `${ID}: ${recipe.title}`,
+          publisher: recipe.publisher,
+        });
+      },
+      1500,
+      recipeID
+    );
   });
 };
 
+const getPublisher = (publisher) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(
+      (pub) => {
+        resolve(`Published by ${pub}`);
+      },
+      1500,
+      publisher
+    );
+  });
+};
 
 // Async and Await are keywords used with a function to make it asynchronous (Async) and to set up key points in the function where it will stop running until a specified promise returns its data (Await).
 
@@ -59,44 +61,57 @@ async function getRecipesAW() {
   console.log(recipe);
   const pub = await getPublisher(recipe.publisher);
   console.log(pub);
-
+  return pub;
 }
 
-getRecipesAW();
+
+// We then call the asynchronous function to invoke it.
+// When we invoke the asynchronous function, we can access the final Resolve data from the final Promlise by putting a 'return' phrase at the end of the async function, the using the .then method to process it.
+
+getRecipesAW().then(pub => console.log(`Remember: You can trust books ${pub}!`));
+
 
 //async/await is a lot neater than the .then chains used in traditional promises.
 
+
+//////////////////////////////////////////////////////////////////////////
+
 // Here's another example using our gumball machine model.
 
-const getGumball = new Promise ((resolve, reject) => {
+const getGumball = new Promise((resolve, reject) => {
   setTimeout(() => {
-      const outcome = Math.floor(Math.random() *10);
-      console.log(`<Gumball Value: ${outcome}>`);
-      outcome >1 ? 
-          resolve(
-              {
-                  message: 'You got a gumball!',
-                  colors: ["red", "blue", "green", "yellow", "pink"],
-                  selector: outcome
-              }
-          ) : reject("Error: The gumball machine jammed!");
+    const outcome = Math.floor(Math.random() * 5);
+    console.log(`<Gumball Value: ${outcome}>`);
+    outcome > 0
+      ? resolve({
+          message: "You got a gumball!",
+          colors: ["red", "blue", "green", "yellow", "pink"],
+          selector: outcome,
+        })
+      : reject("Error: The gumball machine jammed!");
   }, 6000);
 });
 
 const gumballSelector = (gumballSelectorData) => {
   return new Promise((resolve, reject) => {
-    setTimeout((gumballSelectorData) => {
-      console.log((`You got a ${gumballSelectorData.colors[gumballSelectorData.selector]} gumball!`));
-    }, 1500, gumballSelectorData);
+    setTimeout(
+      (gumballSelectorData) => {
+        const gumColor = gumballSelectorData.colors[(gumballSelectorData.selector-1)];
+        console.log(`You got a ${gumColor} gumball!`);
+        resolve(gumColor);
+        reject("Error!")
+      },
+      1500,
+      gumballSelectorData
+    );
   });
 };
 
 async function gumballMachine() {
-    const gumballData = await getGumball;
-    console.log(gumballData.message);
-    const gumballColor = await gumballSelector(gumballData);
-    gumballColor();
+  const gumballData = await getGumball;
+  console.log(gumballData.message);
+  const gumballColor = await gumballSelector(gumballData);
+  return gumballColor;
 }
 
-gumballMachine();
-
+gumballMachine().then(gumballColor => console.log(`${gumballColor} is my favourite color!`));
